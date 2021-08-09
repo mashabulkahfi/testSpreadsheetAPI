5d8208dd1c2c8cf63229b38818e28c8e13fa9c82
Muhammad Ashabul Kahfi <33783920+mashabulkahfi@users.noreply.github.com>
2021-08-09 12:25:01 +0700

    Add files via upload

---
+++ my.js
@@ -0,0 +1,25 @@
+
+function testGS() {
+    const url = "https://script.google.com/macros/s/AKfycbxoJlkIqQS1mzY_ry6-hzsf-jXzwIXN2K7wW18r192Cc8hAYeaeX5byNwp8VQYDcDk/exec";
+
+    fetch(url).then(d => d.json()).then(d => {document.getElementById("app").textContent = d[0].status});
+}
+
+function addGS() {
+    const url = "https://script.google.com/macros/s/AKfycbxoJlkIqQS1mzY_ry6-hzsf-jXzwIXN2K7wW18r192Cc8hAYeaeX5byNwp8VQYDcDk/exec";
+
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({name: "Olivia"})
+    });
+}
+
+document.getElementById("btn2").addEventListener("click", addGS);
+
+document.getElementById("btn").addEventListener("click", testGS);
