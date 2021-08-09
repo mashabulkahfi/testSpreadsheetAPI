372cad1e74468825573dc9cfe4b9980c4a7ac50c
Muhammad Ashabul Kahfi <33783920+mashabulkahfi@users.noreply.github.com>
2021-08-09 15:59:49 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,12 +1,12 @@
 
 function testGS() {
-    const url = "https://script.google.com/macros/s/AKfycbxoJlkIqQS1mzY_ry6-hzsf-jXzwIXN2K7wW18r192Cc8hAYeaeX5byNwp8VQYDcDk/exec";
+    const url = "https://script.google.com/macros/s/AKfycbw6jLsPSw9Jzupr3CqQU6I6bKshYDGW-BFQQ6YOfornZO_B-_K4kswY_EqAczS099n-/exec";
 
     fetch(url).then(d => d.json()).then(d => {document.getElementById("app").textContent = d[0].status});
 }
 
 function addGS() {
-    const url = "https://script.google.com/macros/s/AKfycbxoJlkIqQS1mzY_ry6-hzsf-jXzwIXN2K7wW18r192Cc8hAYeaeX5byNwp8VQYDcDk/exec";
+    const url = "https://script.google.com/macros/s/AKfycbw6jLsPSw9Jzupr3CqQU6I6bKshYDGW-BFQQ6YOfornZO_B-_K4kswY_EqAczS099n-/exec";
 
     fetch(url, {
         method: 'POST',
