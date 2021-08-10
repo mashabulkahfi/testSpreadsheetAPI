ce0136cf078cf3dca88d81b5295e528f4c576874
Muhammad Ashabul Kahfi <33783920+mashabulkahfi@users.noreply.github.com>
2021-08-11 06:54:18 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,12 +1,15 @@
-
 function testGS() {
-    const url = "https://script.google.com/macros/s/AKfycbyyeB0ExG9w1EUkqHMo5rtwRaGTo3lSMYGV6C3-xommKngyioKaNa0Q5A0CmPtcmADG/exec";
+    const url = "https://script.google.com/macros/s/AKfycbwqYq9bnVhDViQtJ6q7KsktD-_ie5L1rmwK55ibEgk0duVAW2XXnIqvrBBfBhTYX7lS/exec";
 
-    fetch(url).then(d => d.json()).then(d => {document.getElementById("app").textContent = d[0].status});
+    fetch(url)
+    .then(d => d.json())
+    .then(d => {
+        document.getElementById("app").textContent = d[0].status;
+    });
 }
 
 function addGS() {
-    const url = "https://script.google.com/macros/s/AKfycbyyeB0ExG9w1EUkqHMo5rtwRaGTo3lSMYGV6C3-xommKngyioKaNa0Q5A0CmPtcmADG/exec";
+    const url = "https://script.google.com/macros/s/AKfycbwqYq9bnVhDViQtJ6q7KsktD-_ie5L1rmwK55ibEgk0duVAW2XXnIqvrBBfBhTYX7lS/exec";
 
     fetch(url, {
         method: 'POST',
@@ -16,10 +19,10 @@ function addGS() {
             'Content-Type': 'application/json'
         },
         redirect: 'follow',
-        body: JSON.stringify({first:"Dzaky",last: "Farhan",phone:"101012992"})
+        body: JSON.stringify({name: "Kahfi"})
     });
 }
 
 document.getElementById("btn2").addEventListener("click", addGS);
 
-document.getElementById("btn").addEventListener("click", testGS);
+document.getElementById("btn1").addEventListener("click", testGS);
