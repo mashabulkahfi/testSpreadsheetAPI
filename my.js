dcc4c500178f1d4bc86d50c461bf37126feea879
Muhammad Ashabul Kahfi <33783920+mashabulkahfi@users.noreply.github.com>
2021-08-11 14:40:25 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,76 +1,29 @@
-function doGet() {
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const ws = ss.getSheetByName("Customers");
-  const data = ws.getRange("A1").getDataRegion().getValues();
-  const headers = data.shift();
+function testGS() {
+    const url = "https://script.google.com/macros/s/AKfycbwqYq9bnVhDViQtJ6q7KsktD-_ie5L1rmwK55ibEgk0duVAW2XXnIqvrBBfBhTYX7lS/exec";
 
-  const jsonArray = data.map(r => {
-    let obj = {};
-    headers.forEach((h, i) => {
-      obj[h] = r[i];
+    fetch(url)
+    .then(d => d.json())
+    .then(d => {
+        document.getElementById("app").textContent = d[0].status;
     });
-    return obj;
-  });
-
-  const response = [{status: 200, data: jsonArray}];
-
-
-  return sendJSON_(response);
 }
 
-function doPost(e) {
-  // {"name": "Joe", "last":"Hoop","phone":"620-000-0001"}
-  let jsonResponse;
+function addGS() {
+    const url = "https://script.google.com/macros/s/AKfycbwqYq9bnVhDViQtJ6q7KsktD-_ie5L1rmwK55ibEgk0duVAW2XXnIqvrBBfBhTYX7lS/exec";
 
-  const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const ws = ss.getSheetByName("Customers");
-  const headers = ws.getRange(1,1,1,ws.getLastColumn()).getValues()[0];
-  const headersOriginalOrder = headers.slice();
-  headersOriginalOrder.shift();
-  //remode id column header
-  headers.shift();
-  headers.sort();
-
-  const body = e.postData.contents;
-  const bodyJSON = JSON.parse(body);
-  const headersPassed = Object.keys(bodyJSON).sort();
-
-  if(!compareTwoArray_(headers, headersPassed)){
-    jsonResponse = {status: 500, message:"Invalid Arguments Passed"};
-    return sendJSON_(jsonResponse);
-  }
-
-  const arrayOfData = headersOriginalOrder.map(h => bodyJSON[h]);
-
-  const aoaIds = ws.getRange(2,1,ws.getLastRow()-1,1).getValues();
-  const newIdNumber = getMaxFromArrayOfArray_(aoaIds) + 1;
-  arrayOfData.unshift(newIdNumber);
-  ws.appendRow(arrayOfData);
-
-  return sendJSON_(jsonResponse)
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({first:"Monica", last:"Momo", phone:"000-111-2222"})
+    });
 }
 
-//return tru if all items are the same
-function compareTwoArray_(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  
-  for(let i=0; i< arr1.length; i++){
-    if(arr1[i] !== arr2[i]) return false;
-  }
+document.getElementById("btn2").addEventListener("click", addGS);
 
-  return true;
-}
+document.getElementById("btn1").addEventListener("click", testGS);
 
-function sendJSON_(jsonResponse){
-  return ContentService
-    .createTextOutput(JSON.stringify(jsonResponse))
-    .setMimeType(ContentService.MimeType.JSON);
-} 
-
-function getMaxFromArrayOfArray_(aoa){
-  let maxID = 0;
-  aoa.forEach(r=> {
-    if(r[0] > maxID) maxID = r[0]
-  });
-  return maxID;
-}
